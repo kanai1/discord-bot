c7ee654e1903eb025d273d47b569dfbe2f1ea2cd
kanai1 <ksh30918@kakao.com>
2025-10-11 03:22:20 +0900

    feat: 기타 수정사항을 반영한 봇 및 서버 실행코드

--- src/main.js
+++ src/main.js
@@ -1,38 +1,31 @@
 require('dotenv').config();
-const fs = require('fs');
 const path = require('path');
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+
+const { getClient } = require(path.join(process.cwd(), 'src/client'));
+const createExpressServer = require(path.join(
+    process.cwd(),
+    'src/server/server'
+));
 const registerCommandHandler = require(path.join(
     process.cwd(),
-    'src/commandHandler'
+    'src/handlers/commandHandler'
 ));
 
-// 봇 클라이언트 생성
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds, // 서버 정보를 가져오기 위해 필요
-        GatewayIntentBits.GuildMessages, // 메시지 이벤트 수신
-        GatewayIntentBits.MessageContent, // 메시지 내용 읽기
-    ],
-});
-
-client.commands = new Collection();
-
-// 명령어 로딩
-const commandFiles = fs
-    .readdirSync(path.join(process.cwd(), 'commands'))
-    .filter((file) => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const command = require(path.join(process.cwd(), `commands/${file}`));
-    client.commands.set(command.data.name, command);
-}
+const client = getClient();
+registerCommandHandler(client);
 
 // 봇이 준비되었을 때
 client.once('clientReady', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-registerCommandHandler(client);
-
 // 봇 로그인
 client.login(process.env.BOT_TOKEN);
+
+// Express 서버 시작
+createExpressServer(client);
+
+process.on('SIGINT', async () => {
+    console.log('봇 종료 중...');
+    process.exit();
+});
