872e42132be501458d22c49c125e32de53fb838b
kanai1 <ksh30918@kakao.com>
2025-10-13 01:39:31 +0900

    fix: 엠베드 형식 수정

--- src/handlers/embedMaker.js
+++ src/handlers/embedMaker.js
@@ -6,10 +6,10 @@ module.exports = function createServerMonitorEmbed(cpuUsage, memUsage, diskUsage
     return new EmbedBuilder()
             .setTitle('서버 상태')
             .addFields(
-                { name: 'CPU 사용량', value: cpuUsage },
-                { name: '메모리 사용량', value: memUsage },
-                { name: '디스크 사용량', value: diskUsage},
-                { name: 'CPU 온도', value: cpuTemp }
+                { name: 'CPU 사용량', value: cpuUsage + '%' },
+                { name: '메모리 사용량', value: memUsage + '%' },
+                { name: '디스크 사용량', value: diskUsage + '%' },
+                { name: 'CPU 온도', value: cpuTemp + '°C' }
             )
             .setColor(BOT_COLOR)
             .setTimestamp();
--- src/handlers/sysinfo.js
+++ src/handlers/sysinfo.js
@@ -1,25 +1,25 @@
 async function getCPUUsage() {
     const res = await fetch('http://glances:61208/api/4/cpu');
     const data = await res.json();
-    return data.total + '%';
+    return data.total;
 }
 
 async function getMemUsage() {
     const res = await fetch('http://glances:61208/api/4/mem');
     const data = await res.json();
-    return data.percent + '%';
+    return data.percent;
 }
 
 async function getDiskUsage() {
     const res = await fetch('http://glances:61208/api/4/fs');
     const data = await res.json();
-    return data[0].percent + '%';
+    return data[0].percent;
 }
 
 async function getCPUTemp() {
     const res = await fetch('http://glances:61208/api/4/sensors');
     const data = await res.json();
-    return data[0].value + '°C';
+    return data[0].value;
 }
 
 module.exports = async function getSystemStats() {
