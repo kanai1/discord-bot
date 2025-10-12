72342ffd2a802430000f2a3d0e36cc0e904ac882
kanai1 <ksh30918@kakao.com>
2025-10-13 00:51:05 +0900

    fix: fetch함수를 async하게 실행 및 병렬수행하도록 수정

--- src/handlers/sysinfo.js
+++ src/handlers/sysinfo.js
@@ -1,33 +1,39 @@
-function getCPUUsage() {
-    const res = fetch('http://glances:61208/api/4/cpu');
+async function getCPUUsage() {
+    const res = await fetch('http://glances:61208/api/4/cpu');
     const data = res.json();
     return data.total + '%';
 }
 
-function getMemUsage() {
-    const res = fetch('http://glances:61208/api/4/mem');
+async function getMemUsage() {
+    const res = await fetch('http://glances:61208/api/4/mem');
     const data = res.json();
     return data.percent + '%';
 }
 
-function getDiskUsage() {
-    const res = fetch('http://glances:61208/api/4/fs');
+async function getDiskUsage() {
+    const res = await fetch('http://glances:61208/api/4/fs');
     const data = res.json();
     return data[0].percent + '%';
 }
 
-function getCPUTemp() {
-    const res = fetch('http://glances:61208/api/4/sensors');
+async function getCPUTemp() {
+    const res = await fetch('http://glances:61208/api/4/sensors');
     const data = res.json();
     return data[0].current + '°C';
 }
 
 module.exports = async function getSystemStats() {
-    
+    const [cpuUsage, memUsage, diskUsage, cpuTemp] = await Promise.all([
+        getCPUUsage(),
+        getMemUsage(),
+        getDiskUsage(),
+        getCPUTemp()
+    ]);
+
     return {
-        cpuUsage: getCPUUsage(),
-        memUsage: getMemUsage(),
-        diskUsage: getDiskUsage(),
-        cpuTemp: getCPUTemp()
+        cpuUsage,
+        memUsage,
+        diskUsage,
+        cpuTemp
     };
 }
