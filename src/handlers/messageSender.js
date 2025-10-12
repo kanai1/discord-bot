9dc7be04251e839a3d17398cfc3a0bf096e5e096
kanai1 <ksh30918@kakao.com>
2025-10-13 01:26:49 +0900

    feat: 유저에게 메시지를 전송하는 핸들러 생성

--- src/handlers/messageSender.js
+++ src/handlers/messageSender.js
@@ -2,18 +2,36 @@ const path = require('path');
 const { getClient } = require(path.join(process.cwd(), 'src/client'));
 client = getClient();
 
-module.exports = async function sendMessage(channelId, message) {
-    try {
-        const channel = await client.channels.fetch(channelId);
-        if (!channel) {
-            throw new Error(`Channel with ID ${channelId} not found`);
-        }
 
-        await channel.send(message);
-        console.log(`Message sent to channel ${channelId}: ${message}`);
-        return { success: true };
-    } catch (err) {
-        console.error(`Failed to send message: ${err.message}`);
-        return { success: false, error: err.message };
+module.exports = { 
+    sendMessagetoChannel: async (channelId, message) => {
+        try {
+            const channel = await client.channels.fetch(channelId);
+            if (!channel) {
+                throw new Error(`Channel with ID ${channelId} not found`);
+            }
+
+            await channel.send(message);
+            console.log(`Message sent to channel ${channelId}: ${message}`);
+            return { success: true };
+        } catch (err) {
+            console.error(`Failed to send message: ${err.message}`);
+            return { success: false, error: err.message };
+        }
+    }, 
+    sendMessagetoUser: async (userID, message) => {
+        try {
+            const user = await client.users.fetch(userID);
+            if (!user) {
+                throw new Error(`User with ID ${userID} not found`);
+            }
+
+            await user.send(message);
+            console.log(`Message sent to user ${userID}: ${message}`);
+            return { success: true };
+        } catch (err) {
+            console.error(`Failed to send message: ${err.message}`);
+            return { success: false, error: err.message };
+        }
     }
 };
--- src/server/routes/test.js
+++ src/server/routes/test.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 
-const sender = require(path.join(process.cwd(), 'src/handlers/messageSender'));
+const {sendMessagetoChannel} = require(path.join(process.cwd(), 'src/handlers/messageSender'));
 
 module.exports = function testRoute() {
     const router = express.Router();
     router.get('/', (req, res) => {
         res.send('Test route is working!');
         try{
-            sender(process.env.TEST_CHANNEL_ID, 'Test message from Express server!');
+            sendMessagetoChannel(process.env.TEST_CHANNEL_ID, 'Test message from Express server!');
         } catch (err) {
             console.error('Failed to send test message:', err);
         }
