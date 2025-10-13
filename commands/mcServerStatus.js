0d51961335884c3ade69b624a18891759dad3212
kanai1 <ksh30918@kakao.com>
2025-10-13 19:47:30 +0900

    feat: 마크서버 상태를 알려주는 명령어 추가

---
+++ commands/mcServerStatus.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const { getRconClient } = require(path.join(process.cwd(), 'src/client'));
+const { createMCServerStatusEmbed } = require(path.join(process.cwd(), 'src/handlers/embedMaker'));
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('mcserverstatus')
+        .setDescription('마인크래프트 서버의 상태를 모니터링합니다.'),
+    async execute(interaction) {
+        await interaction.reply({ content: '🔧 서버 정보를 확인중입니다.', flags: MessageFlags.Ephemeral });
+        const rcon = getRconClient();
+        let response;
+        
+        try {
+            response.tps = await rcon.send('tps');
+            response.players = await rcon.send('list');
+            const match = response.players.match(/There are (\d+) of a max of (\d+) players/);
+            if (match) {
+                response.players = `${match[1]} / ${match[2]}`;
+            } else {
+                console.error('접속자 수를 파악할 수 없습니다.');
+                response.players = '알 수 없음';
+            }
+        } catch (error) {
+            console.error('RCON 명령어 실행 실패:', error);
+            await interaction.editReply({ content: '❌ 서버연결에 실패했습니다. 관리자에게 문의하세요.' });
+            return;
+        }
+
+        const embed = createMCServerStatusEmbed(response.tps, response.players);
+
+        await interaction.editReply({
+            content: '✅ 서버 정보 수집 완료!'
+        });
+
+        await interaction.followUp({ embeds: [embed] });
+    },
+};
--- src/handlers/embedMaker.js
+++ src/handlers/embedMaker.js
@@ -2,15 +2,32 @@ const path = require('path');
 const {EmbedBuilder} = require('discord.js');
 const { BOT_COLOR } = require(path.join(process.cwd(), 'configs/constants'));
 
-module.exports = function createServerMonitorEmbed(cpuUsage, memUsage, diskUsage, cpuTemp) {
+function createServerMonitorEmbed(cpuUsage, memUsage, diskUsage, cpuTemp) {
     return new EmbedBuilder()
-            .setTitle('서버 상태')
-            .addFields(
-                { name: 'CPU 사용량', value: cpuUsage + '%' },
-                { name: '메모리 사용량', value: memUsage + '%' },
-                { name: '디스크 사용량', value: diskUsage + '%' },
-                { name: 'CPU 온도', value: cpuTemp + '°C' }
-            )
-            .setColor(BOT_COLOR)
-            .setTimestamp();
-    }
+        .setTitle('서버 상태')
+        .addFields(
+            { name: 'CPU 사용량', value: cpuUsage + '%' },
+            { name: '메모리 사용량', value: memUsage + '%' },
+            { name: '디스크 사용량', value: diskUsage + '%' },
+            { name: 'CPU 온도', value: cpuTemp + '°C' }
+        )
+        .setColor(BOT_COLOR)
+        .setTimestamp();
+}
+
+function createMCServerStatusEmbed(tps, players) {
+    if (tps == 20) tps = '🟢 20 (최적)';
+    else if (tps >= 16) tps = `🟡 ${tps} (양호)`;
+    else tps = `🔴 ${tps} (불안정)`;
+
+    return new EmbedBuilder()
+        .setTitle('마인크래프트 서버 상태')
+        .addFields(
+            { name: 'TPS', value: tps },
+            { name: '접속자 수', value: players }
+        )
+        .setColor(BOT_COLOR)
+        .setTimestamp();
+}
+
+module.exports = { createServerMonitorEmbed, createMCServerStatusEmbed };
