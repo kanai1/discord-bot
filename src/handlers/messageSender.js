d57cdb851efbefac8c7f92b0e81d31241e64c7ab
kanai1 <ksh30918@kakao.com>
2025-10-14 10:43:12 +0900

    refactor: export 형식 변경

--- src/handlers/messageSender.js
+++ src/handlers/messageSender.js
@@ -1,37 +1,37 @@
 const path = require('path');
 const { getBotClient } = require(path.join(process.cwd(), 'src/client'));
-client = getBotClient();
+const client = getBotClient();
 
-
-module.exports = { 
-    sendMessagetoChannel: async (channelId, message) => {
-        try {
-            const channel = await client.channels.fetch(channelId);
-            if (!channel) {
-                throw new Error(`Channel with ID ${channelId} not found`);
-            }
-
-            await channel.send(message);
-            console.log(`Message sent to channel ${channelId}: ${message}`);
-            return { success: true };
-        } catch (err) {
-            console.error(`Failed to send message: ${err.message}`);
-            return { success: false, error: err.message };
+async function sendMessagetoChannel(channelId, message) {
+    try {
+        const channel = await client.channels.fetch(channelId);
+        if (!channel) {
+            throw new Error(`Channel with ID ${channelId} not found`);
         }
-    }, 
-    sendMessagetoUser: async (userID, message) => {
-        try {
-            const user = await client.users.fetch(userID);
-            if (!user) {
-                throw new Error(`User with ID ${userID} not found`);
-            }
 
-            await user.send(message);
-            console.log(`Message sent to user ${userID}: ${message}`);
-            return { success: true };
-        } catch (err) {
-            console.error(`Failed to send message: ${err.message}`);
-            return { success: false, error: err.message };
-        }
+        await channel.send(message);
+        console.log(`Message sent to channel ${channelId}: ${message}`);
+        return { success: true };
+    } catch (err) {
+        console.error(`Failed to send message: ${err.message}`);
+        return { success: false, error: err.message };
     }
-};
+}
+
+async function sendMessagetoUser (userID, message) {
+    try {
+        const user = await client.users.fetch(userID);
+        if (!user) {
+            throw new Error(`User with ID ${userID} not found`);
+        }
+
+        await user.send(message);
+        console.log(`Message sent to user ${userID}: ${message}`);
+        return { success: true };
+    } catch (err) {
+        console.error(`Failed to send message: ${err.message}`);
+        return { success: false, error: err.message };
+    }
+}
+
+module.exports = { sendMessagetoChannel, sendMessagetoUser };
