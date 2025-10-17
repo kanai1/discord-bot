d1ed24cf6680b3cb8e373d105db7b19e80813554
kanai1 <ksh30918@kakao.com>
2025-10-17 17:36:15 +0900

    feat: 명령어 핸들러 수정
    - 그룹 명령어도 등록할 수 있도록 변경

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -3,15 +3,28 @@ const path = require('path');
 const { Collection } = require('discord.js');
 
 module.exports = (client) => {
-    // 명령어 로딩
-    client.commands = new Collection();
-    const commandFiles = fs
-        .readdirSync(path.join(process.cwd(), 'commands'))
-        .filter((file) => file.endsWith('.js'));
+    // commands 폴더 경로
+    const commandsPath = path.join(process.cwd(), 'commands');
 
-    for (const file of commandFiles) {
-        const command = require(path.join(process.cwd(), `commands/${file}`));
-        client.commands.set(command.data.name, command);
+    // commands 폴더 내부 탐색
+    const commandFolders = fs.readdirSync(commandsPath);
+
+    for (const folder of commandFolders) {
+        const folderPath = path.join(commandsPath, folder);
+
+        // 하위에 여러 파일이 있는 그룹 명령어 폴더일 경우
+        if (fs.statSync(folderPath).isDirectory()) {
+            const commandFiles = path.join(folderPath, 'index.js');
+            if(fs.existsSync(commandFiles)) {
+                const command = require(commandFiles);
+                client.commands.set(command.data.name, command);
+            }
+        } 
+        // 단일 명령어 파일일 경우
+        else if (folder.endsWith('.js')) {
+            const command = require(folderPath);
+            client.commands.set(command.data.name, command);
+        }
     }
 
     // 인터랙션 처리
