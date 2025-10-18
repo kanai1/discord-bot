8f1687ba68889ee567d42014d29e7ad26cebf8ea
kanai1 <ksh30918@kakao.com>
2025-10-18 14:22:54 +0900

    fix: 명령어 typo

--- src/utils/docker.js
+++ src/utils/docker.js
@@ -2,8 +2,8 @@ const { exec } = require('child_process');
 
 function runDockerComposeUp(composePath, projectName) {
     return new Promise((resolve, reject) => {
-        console.log(`docker compose -p ${projectName} - ${composePath} up -d`);
-        exec(`docker compose -p ${projectName} - ${composePath} up -d`, (error, stdout, stderr) => {
+        console.log(`docker compose -p ${projectName} -f ${composePath} up -d`);
+        exec(`docker compose -p ${projectName} -f ${composePath} up -d`, (error, stdout, stderr) => {
             if (error) {
                 reject(stderr || error.message);
             } else {
